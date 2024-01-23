6fd70f89de0efedc26ff577abc3f25018bb3862e
Aquibarain <155629079+Aquibarain@users.noreply.github.com>
2024-01-23 10:20:29 +0400

    Initial commit

---
+++ Mosh/index.js
@@ -0,0 +1,87 @@
+// let name = 'Aqib';
+// console.log(name);
+
+// var interestRate = 0.3;
+//  interestRate = 1;
+//  console.log(interestRate);
+
+// Object
+// let person ={
+//     name: "Aqib",
+//     age: 30,
+
+// };
+// person['name'] = 'Aaquib';
+// console.log(person.name);
+
+
+// Arrays
+// let selectedColors = ['red', 'blue'];
+// selectedColors[2] = '1'
+// console.log(selectedColors.length)
+
+
+
+// Function
+// function greet(name, lastName) {
+//    console.log('Hellow ' + name + ' ' + lastName); 
+// }
+// greet('john', 'Aaquib');
+// greet('mary')
+// greet('aqib')
+
+// Type of Functions
+// function square(number) {
+//     return number * number;
+// }
+
+// console.log(square(2));
+
+
+
+// //Comparison Operators
+
+// let x = 1;
+// //Relational
+// console.log(x > 0);
+// console.log(x >= 1);
+// console.log(x < 1);
+// console.log(x <= 1);
+// //Equality
+// console.log(x === 1);
+// console.log(x !== 1);
+
+
+
+// // Equality Operatars
+
+// // Strict Equality (Type + Value)
+// console.log(1=== 1);
+// console.log('1'=== 1);
+
+// // Lose Equality
+// console.log(1== 1);
+// {}
+// console.log('1'== 1);
+// console.log(true == 1);
+
+
+// // Ternary Operator
+// let points = 110;
+// let type = points > 100 ? 'gold' : 'silver';
+// console.log(type);
+
+// logical OR (||)
+let highIncome =false;
+let goodCreditScore = false;
+let eligibleForloan = highIncome || goodCreditScore;
+console.log ('Eligible',eligibleForloan);
+
+// Not (!)
+let applictionRefused = !eligibleForloan
+console.log('Appliction Refused',applictionRefused)
+
+
+
+
+
---
+++ Scricpt/script.js
@@ -0,0 +1,38 @@
+// Artimetic Operators
+// let a = 5;
+// let b = 2;
+
+// console.log("a + b =",a + b);
+// console.log("a - b =",a - b);
+// console.log("a * b =",a * b);
+// console.log("a / b =",a / b);
+// console.log("a % b =",a % b);
+// console.log("a ** b =",a ** b);
+// console.log("a +- b =",a +- b);
+
+// Unary Operator
+// let a = 5;
+// let b = 2;
+
+// console.log("a =", a,"& b =",b);
+
+// console.log("++a =", ++a);
+// console.log("a =", a);
+// console.log("--b =", --b);
+// console.log("b =", b);
+
+
+// Asignment Operator
+// let a = 5;
+// let b = 2;
+
+// a *= 4;
+
+// console.log("a =", a);
+
+
+// Comparison Operator
+let a = 6;
+let b = 5;
+
+console.log(" 6 >= 5  " ,a >= b )
